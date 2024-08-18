05131f5d4bdf69a2893f1c5e6d9bb0a570341cee
rheajshah <rheajshah@gmail.com>
2024-08-17 17:09:40 -0700

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-// App.tsx
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginScreen from './LoginScreen';
 import SignUpScreen from './SignUpScreen';
-import PracticeList from './PracticeList';
-
-import './App.css';
+import PracticeList from './PracticeList'; // Update the path as necessary
+import './App.css'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LoginScreen />} />
-      <Route path="/signup" element={<SignUpScreen />} />
-      <Route path="/practices" element={<PracticeList />} />
-    </Routes>
+    <div className="App">
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<LoginScreen />} />
+          <Route path="/signup" element={<SignUpScreen />} />
+          <Route path="/practices" element={<PracticeList />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom/client'; // Make sure you use the 'client' import for React 18
 import App from './App';
+import './index.css';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
 );
